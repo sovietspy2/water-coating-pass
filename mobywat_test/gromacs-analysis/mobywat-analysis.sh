4486b5f36580025d8e7bcea775b6fcb001f2c0a6
SovietSpy2 <sovietspy2@yandex.com>
2026-06-14 10:46:28 +0200

    mobywat analysis v3

--- mobywat_test/gromacs-analysis/mobywat-analysis.sh
+++ mobywat_test/gromacs-analysis/mobywat-analysis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# MobyWat GROMACS Trajectory Preprocessor
+# MobyWat GROMACS ANALYSIS
 # Input files: md.trr, md.tpr, system_ref.pdb
 
 gmx trjconv -f md.trr -s md.tpr -o pbc1.xtc -pbc whole << EOF
@@ -16,7 +16,7 @@ gmx trjconv -f pbc2.xtc -s md.tpr -o pbc3.xtc -center -pbc mol -ur compact << EO
 0
 EOF
 
-gmx confrms -one -f1 system_reference.pdb -f2 md.tpr -o fit.pdb << EOF
+gmx confrms -one -f1 system_ref.pdb -f2 md.tpr -o fit.pdb << EOF
 3
 3
 EOF
@@ -33,6 +33,34 @@ gmx trjconv -f pbc3.xtc -s fit.pdb -o system_tpy.pdb -b 0 -e 0 -fit progressive 
 0
 EOF
 
+awk '
+BEGIN {n=1}
+$1=="ATOM" || $1=="HETATM" {
+    printf "%-6s%5d%s\n", substr($0,1,6), n, substr($0,12)
+    n++
+    next
+}
+$1=="TER" {
+    printf "TER\n"
+    next
+}
+{print}
+' system_ref.pdb | sponge system_ref.pdb
+
+sed -i '1i\
+REMARK mobywat_reference_target [A]\
+REMARK mobywat_reference_waters Auto\
+#REMARK mobywat_min_ctol   1.0\
+#REMARK mobywat_num_ctol   4\
+#REMARK mobywat_step_ctol  0.50\
+#REMARK mobywat_min_ptol   2.50\
+#REMARK mobywat_num_ptol   1\
+#REMARK mobywat_step_ptol  0\
+#REMARK mobywat_min_stol   1.500\
+#REMARK mobywat_num_stol   1\
+#REMARK mobywat_step_stol  0
+' "system_ref.pdb"
+
 echo "Done! MobyWat input files ready: system.xtc, system_tpy.pdb, fit.pdb"
 
-mobywat -tpy system_tpy.pdb -t [A] -w Auto -n 0-100 -m Analysis -v Diagnostic
+mobywat -t [A] -w Auto -n 0-1000 -m Analysis -v Diagnostic
