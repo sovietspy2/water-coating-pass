19ee354f71cbd5707dfa52109f8d07bb16d26123
SovietSpy2 <sovietspy2@yandex.com>
2026-06-21 12:51:29 +0200

    removing unused dir

--- mobywat_test/gromacs/mobywat.sh
+++
@@ -1,9 +0,0 @@
-gmx editconf -f mobywat_input.gro -o mobywat_input_tpy.pdb
-
-gmx check -f mobywat_input.xtc
-
-mobywat -f mobywat_input.xtc -tpy mobywat_input_tpy.pdb \
-  -t A -w Auto -n 0-1000 \
-  -m Prediction -cls IDa \
-  -ctol 1.0 -ptol 2.5 -dmax 3.5 -mtol 1.5 -bmax 30.0 \
-  -v Verbose
--- mobywat_test/tinker/mobywat.sh
+++
@@ -1,4 +0,0 @@
-mobywat -f mobywat_input.pdb -t A -w Auto -n 0-100 \
-  -m Prediction -cls IDa \
-  -ctol 1.0 -ptol 2.5 -dmax 3.5 -mtol 1.5 -bmax 30.0 \
-  -v Silent
